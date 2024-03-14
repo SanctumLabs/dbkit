40e2e0d4d018161712714b49e691bbb85d1d04ed
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2024-03-14 14:09:55 +0300

    feat(mixins): table mixins
    Table mixins that provide additional functionality & metadata for database tables
    in a database. The assumption made for this change is the use of PostgreSQL as the
    underlying database, this is especially the case when using the PrimaryKeyMixin.

--- dbkit/pymaze/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .border import Border
-from .maze import Maze
-from .role import Role
-from .solution import Solution
-from .square import Square
-
-__all__ = ["Border", "Maze", "Role", "Solution", "Square"]
