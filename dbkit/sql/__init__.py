3b412440da2029ac3b72c4e16d49ba084c5bf101
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2024-03-22 07:12:48 +0300

    test(dbkit): end to end tests

--- dbkit/sql/__init__.py
+++ dbkit/sql/__init__.py
@@ -1,13 +1,56 @@
+from typing import List, Optional, cast
+
 from sqlalchemy_utils import force_instant_defaults
+from sqlalchemy import Sequence, event
+from sqlalchemy.orm import SessionTransaction, UOWTransaction
 
 from dbkit.sql.session import Session, SessionLocal
-from dbkit.sql.listeners import process_hooks, remove_remaining_hooks, before_flush
 from dbkit.sql.logger import log_sql_statements
 from dbkit.sql.types import CommitCallback
 from dbkit.sql.callbacks import on_commit
+from dbkit.sql.models import BaseModel
+from dbkit.protocols import User
+from dbkit.utils import set_updated_by
 
 # sets defaults for columns in the models on instantiation
 force_instant_defaults()
+
+
+@event.listens_for(SessionLocal, "after_commit")
+def process_hooks(current_session: Session) -> None:
+    """The `after_commit` event triggers when any transaction, root or nested, is committed. We therefore first check
+    if we're in the outermost transaction and only trigger the commit callbacks if so.
+    """
+    if not current_session.in_nested_transaction():
+        hooks = cast(List[CommitCallback], current_session.info.get("on_commit_hooks", []))
+
+        while hooks:
+            fn = hooks.pop(0)
+            fn(current_session)
+
+
+@event.listens_for(SessionLocal, "after_transaction_end")
+def remove_remaining_hooks(current_session: Session, transaction: SessionTransaction) -> None:
+    """
+    The `after_transaction_end` event triggers when a transaction ends, irrespective of whether it committed
+    successfully or ended in a rollback. When this happens, and we're not in a nested transaction, we
+    reset all `on_commit_hooks`.
+    """
+    if not transaction.parent:
+        current_session.info.pop("on_commit_hooks", None)
+
+
+@event.listens_for(SessionLocal, "before_flush")
+def before_flush(current_session: Session, flush_context: UOWTransaction, instances: Optional[Sequence]) -> None:
+    """Hook that sets the updated_by field for entities before flushing the changes with the current session"""
+    if hasattr(current_session, "auth"):
+        user = cast(User, current_session.auth)
+
+        for entities in [current_session.new, current_session.dirty]:
+            for entity in entities:
+                if isinstance(entity, BaseModel):
+                    set_updated_by(user, entity)
+
 
 __all__ = [
     "process_hooks",
@@ -16,5 +59,6 @@ __all__ = [
     "log_sql_statements",
     "on_commit",
     "SessionLocal",
-    "Session"
+    "Session",
+    "CommitCallback"
 ]
