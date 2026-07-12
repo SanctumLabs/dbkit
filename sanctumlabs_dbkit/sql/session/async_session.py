fafad0502a54cb3e0292e13a09156174cb61c9fa
Lusina <12752833+BrianLusina@users.noreply.github.com>
2026-07-12 19:49:56 +0300

    Update sanctumlabs_dbkit/sql/session/async_session.py
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Signed-off-by: Lusina <12752833+BrianLusina@users.noreply.github.com>

--- sanctumlabs_dbkit/sql/session/async_session.py
+++ sanctumlabs_dbkit/sql/session/async_session.py
@@ -56,48 +56,13 @@ class AsyncSession(BaseAsyncSession):
         return cast(FuncT, wrapper)
 
 
-async def async_transaction(func: FuncT) -> FuncT:
+def async_transaction(func: FuncT) -> FuncT:
     """
     A decorator to wrap an instance method within a transaction.
 
     If we are already within a transaction, a nested transaction will be started.
 
     Example:
-
-    ```python
-    from sanctumlabs_dbkit.sql.session import AsyncSessionLocal
-    from sanctumlabs_dbkit.sql.sesison import async_transaction
-
-    class UserService():
-        def __init__(session: AsyncSession):
-            self.session = session
-
-        @async_transaction
-        def create(payload) -> User:
-            user = User(**payload)
-            self.session.add(user)
-
-            return user
-
-    session = AsyncSessionLocal()
-
-    user_service = UserService(session)
-    user_service.create({"first_name": "Bob"})
-    """
-
-    @functools.wraps(func)
-    async def wrapper(self: Any, *args: Any, **kwargs: Any) -> Any:
-        if not self.session or not isinstance(self.session, AsyncSession):
-            # pylint: disable=broad-exception-raised
-            raise Exception(
-                "The @transaction decorator requires that an instance variable `session` be set to an instance of a "
-                "`Session`."
-            )
-
-        async with self.session.begin():
-            return func(self, *args, **kwargs)
-
-    return wrapper
 
 
 AsyncSessionLocal = async_sessionmaker(class_=AsyncSession)
